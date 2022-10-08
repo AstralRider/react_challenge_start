dd2f9fae3714dd7406354c43d4591de7aeedf72f
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-10-08 17:04:15 +1100

    mapped contacts state

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ obj }) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(obj).map((value, idx) => (
+        <p key={idx} className={`${idx === 0 ? "tile-title tile" : "tile"}`}>
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Tile } from "../tile/Tile";
 
 export const TileList = ({ contactArray }) => {
-  contactArray.map(({ name, email, phone }) => console.log(name, email, phone));
   return (
     <div>
-      {contactArray.map(({ name, email, phone }, idx) => (
-        <Tile key={idx} name={name} email={email} phone={phone} />
+      {contactArray.map((array, idx) => (
+        <Tile key={idx} obj={array} />
       ))}
     </div>
   );
